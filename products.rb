b4661b9528b1e94359afc46cba415f65912203c4
Björn Schmidtke <pA1nD@users.noreply.github.com>
2016-03-24 18:12:05 +0100

    Create products.rb

---
+++ products.rb
@@ -0,0 +1,64 @@
+  # Earwarmer
+  products << FeaturedProduct.new(
+    height: 1,
+    width: 1,
+    image: ['featured/180sEarwarner.jpg'],
+    name: 'Bluetooth Earwarmer',
+    brand: '180s',
+    logo: 'featured/180s.png',
+    type: 'Earwarmer',
+    modalImage: ['featured/180sEarwarner.jpg'],
+    descrDE: "
+      180s Earwarmer mit patentiertem behind the head Einstellsystem mit integrierter
+      Bluetooth Technologie (Freisprechanlage), integriertem ACCU bis zu 9 Std. standby und USB Ladekabel.
+    ",
+    descrEN: "",
+    query: '?query=11582-001',
+    priceRange: ''
+  )
+
+  # Thorlo
+  products << FeaturedProduct.new(
+    height: 1,
+    width: 1,
+    image: ['featured/ExperiaXECU.jpg'],
+    name: 'Experia Energy XEOU',
+    brand: 'Thorlo',
+    logo: 'featured/thorlo.png',
+    type: 'Socks',
+    modalImage: ['featured/ExperiaXECU.jpg'],
+    descrDE: "
+      Experia Energie ist die nächste Generation von Experia und die technisch fortschrittlichste Leistungs-Socke der Welt. Experia Energie wurde von engagierten Läufer aller Leistungsstufen getestet.
+      <br><br>
+      Anstrengende Tätigkeiten sind eine Belastung für Sehnen. Kompression im Bogen und Knöchel hilft dabei, Faszie und Achillessehnen zu unterstützen und zu stabilisieren.
+    ",
+    descrEN: "",
+    query: '?query=xeou',
+    priceRange: ''
+  )
+
+  # Wolffepack
+  products << FeaturedProduct.new(
+    height: 2,
+    width: 2,
+    image: ['featured/wolffepack2.jpg', 'featured/wolffepack.jpg'],
+    name: 'Escape',
+    brand: 'Wolffepack',
+    logo: 'featured/wolffepack.png',
+    type: 'Backpack',
+    special: 'ISPO AWARD WINNER 2016',
+    moreSpecial: "The world's first orbital backpack",
+    modalImage: ['featured/wolffepack2.jpg', 'featured/wolffepack.jpg'],
+    descrDE: "
+      REVOLUTIONARY NEW DESIGN. The Wolffepack is the world's first orbital backpack, an award-winning innovation that swings your gear round to the front with the bag strapped to your back.
+      <br>
+      THE PERFECT COMMUTER BACKPACK. The comfort of a backpack or rucksack with the easy access of a satchel, messenger bag or shoulder bag. With waterproof zip and reflector strips.
+    ",
+    descrEN: "
+      REVOLUTIONARY NEW DESIGN. The Wolffepack is the world's first orbital backpack, an award-winning innovation that swings your gear round to the front with the bag strapped to your back.
+      <br><br>
+      THE PERFECT COMMUTER BACKPACK. The comfort of a backpack or rucksack with the easy access of a satchel, messenger bag or shoulder bag. With waterproof zip and reflector strips.
+    ",
+    query: '?query=Wolffepack',
+    priceRange: '90.00 € - 199.00 €'
+  )
